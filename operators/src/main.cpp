74958a6e1114d4ea1ad92f2161969b020f5a8bf9
Itemaro <mvap.me@outlook.comR>
2024-01-25 19:40:01 -0300

    challenge finalized

--- operators/src/main.cpp
+++ operators/src/main.cpp
@@ -4,16 +4,37 @@ using namespace std;
 
 int main() {
 
-  int valor {0};
-  const int real {100};
-  const int cinquenta {50};
-  const int vintecinco {25};
-  const int dez {10};
-  const int cinco {5};
-  const int um {1};
+  const int valor_real{100};
+  const int valor_cinquenta{50};
+  const int valor_vintecinco{25};
+  const int valor_dez{10};
+  const int valor_cinco{5};
+  const int valor_um{1};
+  int valor{};
 
   cout << "Digite o valor total em centavos para separar o troco: ";
   cin >> valor;
 
+  int resto{}, real{}, cinquenta{}, vintecinco{}, dez{}, cinco{}, um{};
+
+  real = valor/valor_real;
+  resto = valor%valor_real;
+  cinquenta = resto/valor_cinquenta;
+  resto %= valor_cinquenta;
+  vintecinco = resto/valor_vintecinco;
+  resto %= valor_vintecinco;
+  dez = resto/valor_dez;
+  resto %= valor_dez;
+  cinco = resto/valor_cinco;
+  resto %= valor_cinco;
+  um = resto/valor_um;
+  resto %= valor_um;
+
+  cout << "Segue a quantidade de troco separada por valores de moedas:" << endl;
+  cout << "Real: " << real << endl << "Cinquenta Centavos: " << cinquenta << endl <<
+  "Vintecinco Centavos: " << vintecinco << endl << "Dez Centavos: " << dez << endl <<
+  "Cinco Centavos: " << cinco << endl << "Um Centavo: " << um << endl;
+  cout << "RESTO: " << resto << endl; 
+  
   return 0;
 }

958213edf86f56ba07db580204f7a81bf747cc83
ItemRaro <mvap.me@outlook.com>
2024-01-25 16:50:44 -0300

    challenge

---
+++ operators/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int valor {0};
+  const int real {100};
+  const int cinquenta {50};
+  const int vintecinco {25};
+  const int dez {10};
+  const int cinco {5};
+  const int um {1};
+
+  cout << "Digite o valor total em centavos para separar o troco: ";
+  cin >> valor;
+
+  return 0;
+}
